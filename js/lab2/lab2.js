0e488987099f526972a972e8931c60830ba1e022
PolehinaKsenia <100567058+PolehinaKsenia@users.noreply.github.com>
2022-05-23 13:20:12 +0300

    Add files via upload

--- js/lab2/lab2.js
+++ js/lab2/lab2.js
@@ -18,11 +18,13 @@ function sumTo(n) {
 //-------------------------------------------------------------------
 /*Этот код возвращает факториал числа n!, используя рекурсивный вызов.*/
 function factorial(n) {
-  if (n == 1) return 1;
-  return n * factorial(n-1);
-  }
+  if (n == 1)
+	return 1;
+	return n * factorial(n-1);  
+}
 //-------------------------------------------------------------------
-/**/
+/*Этот код возвращает n-е число Фибоначчи.
+Возвращаемое значение должно иметь тип BigInt.*/
 function fib(n) {
   let a = 1;
   let b = 1;
@@ -34,3 +36,31 @@ function fib(n) {
   return BigInt(b);
 }
 //-------------------------------------------------------------------
+/*Этот код принимает целочисленное значение x и возвращает анонимную функцию.*/
+function compare(n,m){
+	if(n < m){
+		return true;
+	} else if (n > m){
+		return false;
+	} else {
+		return null;
+	}
+}
+//-------------------------------------------------------------------
+/*Этот код принимает два целочисленных значения:
+	num - конечное число
+	cols - количество колонок
+и выводит числа от 0 до num */
+function printNumbers(num, cols) {
+  let str1 = ""
+  let rows = Math.ceil(num / cols); //кол-во строк округлено в большую сторону
+  for (let row = 0; row < rows; ++row) {
+      let str = '';
+      for (let col = 0; col < cols; ++col) {
+          if ((row + rows * col) < num)
+              str += (row + rows * col) + ' ';
+      }
+      str1+=str+"\n";       
+  }
+  return str1;
+}
