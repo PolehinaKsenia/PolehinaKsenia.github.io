de120f1744680156f0565f2e1c47312eeb5752de
PolehinaKsenia <100567058+PolehinaKsenia@users.noreply.github.com>
2022-06-03 10:59:47 +0300

    lab3js

--- js/lab3/lab3.js
+++ js/lab3/lab3.js
@@ -4,11 +4,7 @@
  * @returns строку
  */
  function getDecimal(num) {
-    var str = "" + num;
-    var zeroPos = str.indexOf(".");
-    if (zeroPos == -1) return 0;
-    str = str.slice(zeroPos);
-    return +str;
+    return num - Math.floor(num);
   }
 //-------------------------------------------------
 /**
